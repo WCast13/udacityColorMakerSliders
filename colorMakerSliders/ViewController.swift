1597a38589f137d917d669a5d7c85c2167957c07
William Castellano <castellkid@aol.com>
2018-05-24 13:10:11 -0400

    Practice with Slider Values

--- colorMakerSliders/ViewController.swift
+++ colorMakerSliders/ViewController.swift
@@ -10,16 +10,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  @IBOutlet var redSlider: UISlider!
+  @IBOutlet var greenSlider: UISlider!
+  @IBOutlet var blueSlider: UISlider!
+  
+  @IBOutlet var colorView: UIView!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    colorView.layer.borderColor = UIColor.white.cgColor
+    colorView.layer.borderWidth = 5
+    
+    redSlider.minimumTrackTintColor = UIColor.red
+    blueSlider.minimumTrackTintColor = UIColor.blue
+    greenSlider.minimumTrackTintColor = UIColor.green
+    
+    setColor(red: redSlider.value, green: greenSlider.value, blue: blueSlider.value)
   }
-
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
+  
+  @IBAction func sliderChanged(_ sender: Any) {
+    setColor(red: redSlider.value, green: greenSlider.value, blue: blueSlider.value)
   }
-
-
+  
+  
+  func setColor(red: Float, green: Float, blue: Float) {
+    
+    let CGred = CGFloat(red)
+    let CGgreen = CGFloat(green)
+    let CGblue = CGFloat(blue)
+    
+    colorView.backgroundColor = UIColor(displayP3Red: CGred, green: CGgreen, blue: CGblue, alpha: 1)
+  }
 }
 
